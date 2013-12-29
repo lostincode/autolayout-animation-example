45fe0671b681f5c8cd29a782cde8b780fb2f59d9
Bill <roundedvision@gmail.com>
2013-12-29 13:14:07 -0800

    added iboutlets and example code

--- autolayout-animation-example/ViewController.m
+++ autolayout-animation-example/ViewController.m
@@ -9,6 +9,9 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIView *squareView;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *verticalConstraint;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *horizontalConstraint;
 
 @end
 
@@ -24,6 +27,34 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - UIButton Actions -
+
+- (IBAction)moveButtonAction:(id)sender
+{
+    CGPoint randomPoint = [self randomPointInView];
+    self.verticalConstraint.constant = randomPoint.y;
+    self.horizontalConstraint.constant = randomPoint.x;
+    
+    [UIView animateWithDuration:.5 delay:0 options:UIViewAnimationOptionBeginFromCurrentState animations:^{
+        [self.squareView layoutIfNeeded];
+    
+    } completion:^(BOOL finished) {
+        
+    }];
+}
+
+#pragma mark - Private -
+
+- (CGPoint)randomPointInView
+{
+    CGPoint p;
+    
+    p.x = arc4random() % ((int)CGRectGetWidth(self.view.frame) - (int)CGRectGetWidth(self.squareView.frame));
+    p.y = arc4random() % ((int)CGRectGetHeight(self.view.frame) - (int)CGRectGetHeight(self.squareView.frame));
+    
+    return p;
 }
 
 @end
